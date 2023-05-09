9425813db0811523398806e99ea235ec7c395ba4
samuel <samuel@PC1-SAMU>
2023-05-09 19:25:59 +0200

    Commit Inicial

---
+++ src/TheLastLeaf.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ *   Programa The Last Leaf
+ */
+
+    public class TheLastLeaf {
+
+    public static void main(String[] args) {
+
+        System.out.println("CKS STUDIO");
+        System.out.println("The last leaf, el mejor juego de la historia.");
+        
+    }
+    
+}
