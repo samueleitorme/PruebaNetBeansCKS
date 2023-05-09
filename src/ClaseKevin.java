b2bd72bc4321dd06814ff735eeee9dc304c32a8e
Kevin <kevnmm96@gmail.com>
2023-05-09 19:36:37 +0200

    Mi primer commit lleva la clase Kevin

---
+++ src/ClaseKevin.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/ClaseJava.java to edit this template
+ */
+
+/**
+ * Clase de Kevin
+ * @author Kevin
+ */
+public class ClaseKevin {
+
+    public ClaseKevin(){   
+    }
+    public void saludo(){
+        System.out.println("Hola bienvenidos");
+    }
+    
+    
+    @Override
+    public String toString(){
+        return "Hola me llamo Kevin";
+    }
+    
+    
+}
